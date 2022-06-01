443a04b0056b1cf8401edcb9b4fa70892b290e47
Mrinalini1008 <mrinalin@iitk.ac.in>
2022-06-01 17:48:33 +0530

    Updates

---
+++ precache-manifest.a9e9ebcc8b8ba9d5116f286b23cef366.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8db8af29dfb9291c1477",
+    "url": "/static/css/main.a4233492.chunk.css"
+  },
+  {
+    "revision": "8db8af29dfb9291c1477",
+    "url": "/static/js/main.0f6994f7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b2bf7c10f0eaf5781adc",
+    "url": "/static/js/2.e32459e9.chunk.js"
+  },
+  {
+    "revision": "770ba1d6276987b68b818ec6ca187826",
+    "url": "/index.html"
+  }
+];
